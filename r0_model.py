4e9cf81fedc7dc80b889d786c68b2786b973237f
Daniel Comber-Todd <daniel.comber.todd@gmail.com>
2021-07-13 18:38:48 +0600

    Refactored date validation. Cleaned up creation of output data frame.

--- r0_model.py
+++ r0_model.py
@@ -8,11 +8,47 @@ import datetime
 import numpy as np
 import pandas as pd
 import sys
-
-
-from params import *
+import params
 
 DATE_FMT = '%d-%m-%Y'
+
+def check_dates(start, data_start, input_start, end, data_end, input_end,
+                window_length):
+    """Checks if user start and end dates (for R0 calculation) are valid given
+    the data start and end dates and R0_WINDOW value"""
+
+    # note that start is <= input_start
+    if input_start > data_end:
+        err_mesg = ("\nSelected dates are not covered by data.\nYour start "
+                    "date is %s, End date of data is %s.")
+        err_data = tuple(map(lambda x: x.strftime(DATE_FMT) if x else None,\
+                                           (input_start, data_end)))
+        raise ValueError(err_mesg % err_data)
+
+    if (start < data_start)  or\
+            (end < data_start or end > data_end):
+        err_mesg = ("\nSelected dates are not covered by data.\n Your start "
+            "and end dates are     %s to %s, \n which requires data running "
+            "from %s to %s. \n Start and end dates of data are "
+            " %s to %s. \n Selected R0_WINDOW is %d days.")
+        err_data = list(map(lambda x: x.strftime(DATE_FMT) if x else None,\
+                                   (input_start, input_end, start, end,\
+                                    data_start, data_end)))
+        err_data.append(window_length)
+        err_data = tuple(err_data)
+        raise ValueError(err_mesg % err_data)
+
+
+# This check is not needed - we just need enough data before the user's start
+# date -- Daniel 13.07
+#    # Length of calculation period
+#    n = (end - start).days
+#    if  n < window_length:
+#        err_str = """The number of days (%d) between start and end dates is \
+#            less than R0_WINDOW (%d).\n Please select valid start and end \
+#            dates"""
+#        err_params = (n, window_length)
+#        raise ValueError(err_str % err_params)
 
 
 def calculate_r0(args):
@@ -21,18 +57,23 @@ def calculate_r0(args):
     output_file = args.output_file
     input_start = pd.to_datetime(args.start)
     input_end = pd.to_datetime(args.end)
-
     if args.window:
         R0_WINDOW = args.window
+    else:
+        R0_WINDOW = params.R0_WINDOW
+    SERIAL_INT = params.SERIAL_INT
+    SUBREGIONS = params.SUBREGIONS
     # Load data
     df = pd.read_csv(input_file)
 
     # Length of entire dataframe
     n = len(df)
 
-    # Get dates. Convert everything to datetime object
+    # Get date, subregion names. Convert everything to datetime object
     dates = pd.to_datetime(df['date'])
-    output = pd.DataFrame(0, index=dates, columns=SUBREGIONS)
+    # first column is date
+    subregions = list(df.columns[1:])
+
 
     # Get start and end dates of per100K data
     data_start = dates.iloc[0]
@@ -49,44 +90,25 @@ def calculate_r0(args):
     #start_R0_date = start + datetime.timedelta(days=R0_WINDOW)
     end = input_end if input_end else data_end
 
+    # Check dates
+    check_dates(start, data_start, input_start, end, data_end, input_end,
+                R0_WINDOW)
     # Select portion of dataframe corresponding to start and end dates
-    # print(' %s, %s, %s, %s' % tuple(map(lambda x: x.strftime(DATE_FMT), (input_start, input_end, start, end))))
-
-    if start < data_start or start > data_end  or\
-            input_end and (input_end < data_start or input_end > data_end):
-        err_mesg = ("\nSelected dates are not covered by data.\n Your start "
-            "and end dates are     %s to %s, \n which requires data running "
-            "from %s to %s. \n Start and end dates of data are "
-            " %s to %s. \n Selected R0_WINDOW is %d days.")
-        err_data = map(lambda x: x.strftime(DATE_FMT) if x else None,\
-                                   (input_start, input_end, start, end,\
-                                    data_start, data_end))
-        err_data.append(R0_WINDOW)
-        err_data = tuple(err_data)
-        print(err_data)
-        raise ValueError(err_mesg % err_data)
     mask = (dates >= start) & (dates <= end)
     df = df.loc[mask]
-
-    # This is the length of the truncated dataframe
     n = len(df)
 
-    if n < R0_WINDOW:
-        err_str = """The number of days (%d) between start and end dates is \
-            less than R0_WINDOW (%d).\n Please select valid start and end \
-            dates"""
-        err_params = ((end - start).days, R0_WINDOW)
-        raise ValueError(err_str % err_params)
+    # list to store output
+    output_list = []
 
-    ans = []
     for t in range(n - R0_WINDOW + 1):
+        day = df['date'].iloc[t + R0_WINDOW-1]
+        new_row = [day]
 
-        day = [df['date'].iloc[t + R0_WINDOW-1]]
-
-        for s in SUBREGIONS:
+        for s in subregions:
             x = [i for i in range(R0_WINDOW)]
 
-            # Take log of data befor doing OLS
+            # Take log of data before doing OLS
             y = np.log(df[s].iloc[t:t + R0_WINDOW])
 
             X = np.vstack([x, np.ones(len(x))]).T
@@ -100,13 +122,14 @@ def calculate_r0(args):
             b, ln_a = lstsq_output[0]
             r_0 = np.exp(b*SERIAL_INT)
 
-            # print(s + ': {:.3f}'.format(r_0))
-            day.append(r_0)
-
-        ans.append(day)
-    output = pd.DataFrame(ans, columns=['date'] + SUBREGIONS).round(3)
+            new_row.append(r_0)
+        output_list.append(new_row)
+    #create output DF and make the date column the index
+    output = pd.DataFrame(output_list, columns=['date'] + subregions)
+    output.set_index('date')
 
     # Save output of r0_values to csv file
+    output = output.round(3)
     output.to_csv(output_file)
 
     return output
@@ -116,21 +139,24 @@ if __name__ == '__main__':
                                      values.')
     parser.add_argument('input_file', type=str, help='Path to data file.')
     parser.add_argument('output_file', type=str, help='Path to output file.')
-    parser.add_argument('--start', type=str, help=("The start date"
-                        "(format dd-mm-yyyy) to compute R_0 values for. The "
-                        "default is R0_WINDOW days from the "
-                        "start of the file. Note that the start date must be "
-                        "at least R0_WINDOW-1 days later than the start of "
-                        "the input file."))
-    parser.add_argument('--end', type=str, help=("The end date "
-                        "(format dd-mm-yyyy) to compute R_0 values for. The "
-                        "default is the end of the file."))
-    parser.add_argument('--window', type=int, help=("Time window to fit R0"
-                                                    " over. Default is 14 days"))
+    parser.add_argument('--start', type=str,
+                        help=("The start date (format dd-mm-yyyy) to compute "
+                              "R_0 values for. The default is R0_WINDOW days "
+                              "from the start of the file. Note that the start"
+                              "date must be at least R0_WINDOW-1 days later "
+                              "than the start of the input file."))
+    parser.add_argument('--end', type=str,
+                        help=("The end date (format dd-mm-yyyy) to compute R_0"
+                              "values for. The default is the end of the file."))
+    parser.add_argument('--window', type=int,
+                        help=("Time window to fit R0 over. Default is 14 days"))
+    parser.add_argument('--diag', type=bool, default=False,
+                        help=("Diagnostic flag. If True, both outputs from the"
+                              "least squares fit will be output."))
 
     args = parser.parse_args()
 
-    # parse validate date
+    # parse and validate dates
     if args.start:
         try:
             args.start = datetime.datetime.strptime(args.start, DATE_FMT)
